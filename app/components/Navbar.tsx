90d682155367c2f3989f2b56d51ef2a5184a460e
Hamzy <112537750+hamzyselamat@users.noreply.github.com>
2025-03-19 00:50:51 +0800

    change mobileNav transition

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -59,9 +59,9 @@ const Navbar = () => {
         <AnimatePresence initial={false}>
           {isVisible ? (
             <motion.div
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0 }}
+              initial={{ opacity: 0 }}//, scale: 0 }}
+              animate={{ opacity: 1 }}//, scale: 1 }}
+              exit={{ opacity: 0 }}//, scale: 0 }}
               transition={{ duration: .2 }}
               className={styles.mask}
               key="box"
