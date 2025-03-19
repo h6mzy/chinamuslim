44cf327e042a8aa89c4471ee8eb599ac1ecbf576
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-03-19 08:47:27 +0000

    wip

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,10 +2,11 @@
 
 import dayjs from 'dayjs'
 import { project_details } from '../_lib/project'
+import styles from './Layout.module.sass'
 
 const SiteFooter = () => {
   return (
-    <div className='pad-x'>
+    <div className={styles.pad}>
       <span>&copy; {dayjs().format('YYYY')} {project_details.title}</span>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 'use client'
 
+import styles from './components/Home.module.sass'
+
 export default function Home() {
   return (
     <main>
-      <section className='container'>
-        <div className='pad' style={{ height: '150dvh' }}>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
+      <section>
+        <div className={styles.container}>
+          <video className={styles.video} src="/videos/home.mp4"  autoPlay  loop  muted  playsInline />
+          <div className={styles.content}>
+            <h1>Zakat 2025</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <button>More info</button>
+          </div>
         </div>
       </section>
     </main>
