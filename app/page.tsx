df7e68aa8f0fba96ec1589873f81ba4e1f204d6e
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-03-20 04:15:18 +0000

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import styles from './components/Home.module.sass'
+import layoutStyles from './components/Layout.module.sass'
 
 export default function Home() {
   return (
@@ -9,9 +10,14 @@ export default function Home() {
         <div className={styles.container}>
           <video className={styles.video} src="/videos/home.mp4"  autoPlay  loop  muted  playsInline />
           <div className={styles.content}>
-            <h1>Zakat 2025</h1>
+            <h2>Zakat 2025</h2>
             <p>Lorem ipsum dolor sit amet.</p>
-            <button>More info</button>
+            <button
+              name="More info"
+              className={layoutStyles.whiteButton}
+            >
+              More info
+            </button>
           </div>
         </div>
       </section>
