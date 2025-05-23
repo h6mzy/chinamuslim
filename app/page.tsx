5cd1c2ae362a65994310374da4498fefea626f13
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-05-23 06:44:04 +0000

    video component

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 'use client'
 
-import styles from '@/styles/Home.module.sass'
 import SplashScreen from '@/components/SplashScreen'
+import FullPageVideo from '@/components/FullPageVideo'
 
 export default function HomePage() {
   return (
     <main>
       <section>
-        <div className={styles.container}>
-          <video className={styles.video} autoPlay loop muted playsInline>
-            <source src="/videos/home.mp4" type="video/mp4" />
-            Your browser does not support the video tag. 
-          </video>
-          <div className={styles.content}>
-            <h2>Qurban 2025</h2>
-            <p>Demonstrate your gratitude to Allah this Eid al-Adha.</p>
-            <button
-              aria-label="More info"
-              className="whiteButton"
-            >
-              More info
-            </button>
-          </div>
-        </div>
-        <SplashScreen />
+        <FullPageVideo />
       </section>
+      <SplashScreen />
     </main>
   )
 }
---
+++ components/FullPageVideo/index.tsx
@@ -0,0 +1,24 @@
+import styles from './FullPageVideo.module.sass'
+
+const FullPageVideo = () => {
+  return (
+    <div className={styles.container}>
+      <video className={styles.video} autoPlay loop muted playsInline>
+        <source src="/videos/home.mp4" type="video/mp4" />
+        Your browser does not support the video tag. 
+      </video>
+      <div className={styles.content}>
+        <h2>Qurban 2025</h2>
+        <p>Demonstrate your gratitude to Allah this Eid al-Adha.</p>
+        <button
+          aria-label="More info"
+          className="whiteButton"
+        >
+          More info
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default FullPageVideo
