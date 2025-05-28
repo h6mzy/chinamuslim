bf4c5ed133ee5efa7be71067aca5705916e110a5
h6mzy <112537750+h6mzy@users.noreply.github.com>
2025-05-28 06:37:06 +0000

    styled cards

--- components/ProjectsGrid/index.tsx
+++ components/ProjectsGrid/index.tsx
@@ -6,8 +6,16 @@ export default function ProjectsGrid() {
   return (
     <div className="grid">
       {projects.map((project, index) => 
-        <Link key={index} href={`/project/${project.id}`}>
-          <Card />
+        <Link
+          key={index}
+          href={`/project/${project.id}`}
+          className="unstyledLink"
+        >
+          <Card
+            title={project.title}
+            label={project.year}
+            photoUrl={project.photoUrl}
+          />
         </Link>
       )}
     </div>
